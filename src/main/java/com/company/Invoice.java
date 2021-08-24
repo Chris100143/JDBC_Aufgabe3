7ffeb1aa91eb2a59e63a275a93d2bfd62937b9a6
Christoph Lottersberger <clottersberger@tsn.at>
2021-08-24 11:41:12 +0200

    Methoden hinzugefügt

---
+++ src/main/java/com/company/Invoice.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Date;
+
+public class Invoice {
+
+    //Daten anzeigen
+    public static void showInvoices()
+    {
+
+    }
+
+    //Daten inserten
+    public static void insertInvoice(Date date, String description, double value, Boolean paid)
+    {
+
+    }
+
+    //Daten updaten
+    public static void updateInvoice(int id, Date date, String description, double value, Boolean paid)
+    {
+
+    }
+
+    //Daten löschen
+    public static void deleteInvoice(int id)
+    {
+
+    }
+
+}
