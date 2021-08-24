ce0a91d3bd1178f77931b791a94a72f6b127b052
Christoph Lottersberger <clottersberger@tsn.at>
2021-08-24 18:38:30 +0200

    Methode insertInvoice implementiert

--- src/main/java/com/company/Invoice.java
+++ src/main/java/com/company/Invoice.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.util.Date;
+import java.sql.Date;
 import java.sql.*;
 
 public class Invoice {
@@ -10,32 +10,69 @@ public class Invoice {
     {
         try
         {
+            //Treiber
             Class.forName("com.mysql.cj.jdbc.Driver");
 
+            //Connection
             Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb","root","");
 
+            //Statement
             Statement stmt = connect.createStatement();
 
-            ResultSet result = stmt.executeQuery("SELECT * FROM tblinvoice;");
+            //SQL ausführen
+            ResultSet result = stmt.executeQuery("SELECT * FROM userdb.tblinvoice;");
 
+            //Ergebnis ausgeben
             while(result.next())
             {
                 System.out.println("Id: " +"\t" + result.getInt(1) + "\n" + "Date: " + "\t" + result.getDate(2) + "\n" + "Description: " + "\t" + result.getString(3) + "\n" + "Value: " + "\t" + result.getString(4) + "\n" + "Paid: " + "\t" + result.getBoolean(5) + "\n");
             }
 
+            //Verbindung schließen
             connect.close();
         }
 
+        //Fehlermeldung
         catch(Exception e)
         {
             System.out.println(e);
         }
     }
 
-    //Daten inserten
+    //Daten einfügen
     public static void insertInvoice(Date date, String description, double value, Boolean paid)
     {
+        try
+        {
+            //Treiber
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
+            //Connection
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb","root","");
+
+            //Statement
+            String query = "insert into userdb.tblinvoice (date, description, value, paid) VALUES (?, ?, ?, ?)";
+            PreparedStatement stmt = connect.prepareStatement(query);
+
+            //Daten setzen
+            stmt.setDate(1, date);
+            stmt.setString(2, description);
+            stmt.setDouble(3, value);
+            stmt.setBoolean(4, paid);
+
+            //SQL ausführen
+            stmt.executeUpdate();
+
+            //Verbindung schließen
+            stmt.close();
+            connect.close();
+        }
+
+        //Fehlermeldung
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 
     //Daten updaten
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,10 +1,17 @@
 package com.company;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
 
         //Methoden ausführen
-        Invoice.showInvoices();
+//        Invoice.showInvoices();
+        Invoice.insertInvoice(Date.valueOf(LocalDate.now()), "Rechnung4", 30.50, false);
+//        Invoice.updateInvoice();
+//        Invoice.deleteInvoice();
     }
 }
