f44010e1dd8286ebd9091f45b523ae8a5372a171
Christoph Lottersberger <clottersberger@tsn.at>
2021-08-24 14:05:19 +0200

    Methode showInvoices implementiert

--- src/main/java/com/company/Invoice.java
+++ src/main/java/com/company/Invoice.java
@@ -1,13 +1,35 @@
 package com.company;
 
 import java.util.Date;
+import java.sql.*;
 
 public class Invoice {
 
     //Daten anzeigen
     public static void showInvoices()
     {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb","root","");
+
+            Statement stmt = connect.createStatement();
+
+            ResultSet result = stmt.executeQuery("SELECT * FROM tblinvoice;");
+
+            while(result.next())
+            {
+                System.out.println("Id: " +"\t" + result.getInt(1) + "\n" + "Date: " + "\t" + result.getDate(2) + "\n" + "Description: " + "\t" + result.getString(3) + "\n" + "Value: " + "\t" + result.getString(4) + "\n" + "Paid: " + "\t" + result.getBoolean(5) + "\n");
+            }
+
+            connect.close();
+        }
+
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 
     //Daten inserten
